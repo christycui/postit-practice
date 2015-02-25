93302f19d41ce6e5e68dfb939930b5d1db364182
christycui <christycui14@gmail.com>
2015-02-25 19:32:45 +0000

    completed category MVC

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,19 @@
+class CategoriesController < ApplicationController
+  def show
+    @category = Category.find(params[:id])
+  end
+  
+  def new
+    @category = Category.new
+  end
+  
+  def create
+    @category = Category.new(params.require(:category).permit(:name))
+    if @category.save
+      flash[:success] = 'New category created.'
+      redirect_to posts_path
+    else
+      render 'new'
+    end
+  end
+end
